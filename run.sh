62c341b73195818300984ccf12a5996bbcce2fc6
Albert Fotso <legrandpone1@gmail.com>
2025-10-15 16:40:14 +0100

    Ajout des variables d'environements

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ set -o nounset
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD --noinput
+python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME --noinput
