c6d3b8b66d9aa6c51c2bac7c5eb5db055e889d1d
Albert Fotso <legrandpone1@gmail.com>
2025-10-15 17:16:28 +0100

    Ajout du fichier build.sh pour le deploiement

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+pip install -r requirements.txt
+npm install
+npm run build
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME --noinput
